f7e010ca9747a56a25add75fb57c3e70d215761b
Vladimir Medvedev <v.megvegb@yandex.ru>
2020-03-02 21:06:21 +0300

    added method isInteger

--- src/task_2/Start.java
+++ src/task_2/Start.java
@@ -22,7 +22,7 @@ public class Start {
             test(arr);
         } catch (MyArraySizeException e) {
             e.printStackTrace();
-        }finally {
+        } finally {
             fillArray(arr);
         }
     }
@@ -31,24 +31,44 @@ public class Start {
         System.out.println("Пожалуйста создайте двумерный массив размером " + correctSIZE + " x " + correctSIZE);
         stringSize = sc.nextInt();
         columnSize = sc.nextInt();
-
     }
 
     public static void test(String[][] arr) throws MyArraySizeException {
-            if (arr.length != correctSIZE) { // обработать позже, если вводить 2 и 3
-                throw new MyArraySizeException("Размер массива некорректен - " + arr.length + " x " + arr.length, arr.length);
-            }
+        if (arr.length != correctSIZE) { // обработать позже, если вводить 2 и 3
+            throw new MyArraySizeException("Размер массива некорректен - " + arr.length + " x " + arr.length, arr.length);
         }
+    }
 
-
-    public static void fillArray(String [][] arr){ // печатается на одно значение меньше чем должно
+    public static void fillArray(String[][] arr) { // печатается на одно значение меньше чем должно
         System.out.println("Теперь введите числовые значения для заполнения строк,\n" +
                 "в данном случае значений будет " + stringSize * columnSize);
-        for (int i = 0; i < stringSize; i++) {
+        for (int i = 0; i < columnSize; i++) {
             for (int j = 0; j < columnSize; j++) {
-                arr[i][j] = sc.nextLine();
+                arr[i][j] = sc.next();
             }
         }
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                if (isInteger(arr[i][j])) {
+                    System.out.println(arr[i][j]);
+//                    int num = Integer.parseInt(arr[i][j]);//  System.out.println(num);
+                } else System.out.println("Обнаружен символ");
+            }
+        }
+    }
+
+    public static boolean isInteger(String s) {
+        boolean result = false;
+        if (!s.isEmpty()) {
+            for (int i = 0; i < s.length(); i++) {
+                if (!Character.isLetter(s.charAt(i))) {
+                    result = true;
+                } else
+                    result = false;
+            }
+        }
+        return result;
     }
 }
 
