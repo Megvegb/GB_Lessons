248464a81282aa0af0e92e2e364c33f5c122de75
Vladimir Medvedev <v.megvegb@yandex.ru>
2020-03-06 22:56:56 +0300

    added task_3

--- src/task_3/CollectionTest.java
+++ src/task_3/CollectionTest.java
@@ -1,11 +1,19 @@
+/**
+ * @author Vladimir Medvedev
+ *  @version dated March 6, 2020
+ *  @task 3.1
+ */
+
 package task_3;
 
 import java.util.*;
 
 public class CollectionTest {
+    private static PhoneBook phoneBook;
+
     public static void main(String[] args) {
-        findString(); // task_3.1
-        phoneBook(); // task 3.2
+//        findString(); // task_3.1
+        phoneBookInit(); // task 3.2
 
     }
 
@@ -17,7 +25,7 @@ public class CollectionTest {
                 "printer", "keyboard", "key", "mouse", "computer", "projector");
 
         Collections.sort(words);
-        LinkedHashMap<String, Integer> map = new LinkedHashMap<>();
+        Map<String, Integer> map = new LinkedHashMap<>();
         String result = "";
         int count = 0;
         for (int i = 1; i < words.size(); i++) {
@@ -37,10 +45,25 @@ public class CollectionTest {
                     " - " + entry.getValue());
     }
 
-    private static void phoneBook() {
+    private static void phoneBookInit() {
+        phoneBook = new PhoneBook();
+
+        phoneBook.add("Медведев", "+74956783456");
+        phoneBook.add("Орлов", "+74997576565");
+        phoneBook.add("Попов", "+79670153345");
+        phoneBook.add("Попов", "+73645671234");
+        phoneBook.add("Медведев", "+79196765433");
+        phoneBook.add("Медведев", "+32587745656");
+        phoneBook.add("Данилов", "8800");
+
+        printInfo();
     }
+    public static void printInfo() {
+        String[] arr = {"Медведев", "Орлов", "Попов","Данилов"};
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i] + " " + phoneBook.get(arr[i]));
 
-
-
+        }
+    }
 }
 
